bd66299970055b295bdd6a3eebe1f28dda5bbc3e
jp-773 <jparrett@gmail.com>
2025-04-16 23:10:15 -0500

    final v4

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from stats import get_num_words
 from stats import get_char_count
 from stats import clean_sort
 
+import sys
+
 def main():
-    book_path = "./books/frankenstein.txt"
+
+    
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     char_count = get_char_count(text)
